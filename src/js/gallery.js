e1f93621cbbc3cfd1f547213b51f4cf4595af9cf
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-04-30 20:18:08 +0200

    1

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,4 +1,11 @@
-import SimpleLightBox from "simplelightbox";
+import SimpleLightBox from 'simplelightbox';
 
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(SimpleLightBox);
+let newGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
+newGallery.on('show.simplelightbox', function () {});
