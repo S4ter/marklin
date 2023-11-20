89c24cea6f9c9f171dcafe205c94caa08f54464c
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-11-20 14:57:39 +0100

    main

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//FAQ Panels
+const acc = document.getElementsByClassName('accordion');
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    this.parentElement.classList.toggle('active');
+
+    let panel = this.nextElementSibling;
+
+    if (panel.style.display === 'block') {
+      panel.style.display = 'none';
+    } else {
+      panel.style.display = 'block';
+    }
+  });
+}
+// -----------
