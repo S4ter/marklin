748691c152df1fdd0d2a83ec72411dbc266fb655
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-11-29 16:43:21 +0100

    hamburger menu

--- index.js
+++ index.js
@@ -17,3 +17,50 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 // -----------
+// smooth scroll to anchor
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+// -----------
+// Info on hover over paint icon
+const testhover = document.getElementById('test');
+const paintsvg = document.getElementById('paintsvg');
+
+paintsvg.addEventListener('mouseover', e => {
+  testhover.style.display = 'block';
+  testhover.style.top = e.clientY - 30 + 'px';
+  testhover.style.left = e.clientX - 150 + 'px';
+});
+paintsvg.addEventListener('mouseleave', () => {
+  setTimeout(() => {
+    testhover.style.display = 'none';
+  }, 50);
+});
+const paintsvg2 = document.getElementById('paintsvg2');
+
+paintsvg2.addEventListener('mouseover', e => {
+  testhover.style.display = 'block';
+  testhover.style.top = e.clientY - 30 + 'px';
+  testhover.style.left = e.clientX - 150 + 'px';
+});
+paintsvg2.addEventListener('mouseleave', () => {
+  setTimeout(() => {
+    testhover.style.display = 'none';
+  }, 50);
+});
+// -----------
+// Hamburger menu
+window.onload = function () {
+  const menu_btn = document.querySelector('.hamburger');
+  const mobile_nav = document.querySelector('.mobile_nav');
+  menu_btn.addEventListener('click', function () {
+    menu_btn.classList.toggle('is-active');
+    mobile_nav.classList.toggle('is-active');
+  });
+};
